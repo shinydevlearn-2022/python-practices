4809c6d1c2561fef4ac782fdf7cc426f6ba01ca9
shinykuchi <kuchipudishiny1@gmail.com>
2024-10-12 00:40:36 +0530

    added variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+x=50
+y=10
+z=20
+"""
+Performing the 
+addition funtion
+
+"""
+#performing addition funtion
+result = x + y + z
+print("addition of numbers is:", result)
+
+a = "Doremon"
+b = "Shinchan"
+c = "Bangtan"
+
+print(a,b,c, '\n')
+
+
