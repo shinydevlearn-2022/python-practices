eda51c7e9efc74a09c7dfdb00d4a9f6a87fea7ad
shinykuchi <kuchipudishiny1@gmail.com>
2024-10-11 17:36:33 +0530

    added multiline comments

--- comments.py
+++ comments.py
@@ -2,3 +2,8 @@
 print("Hello, world!")
 print("I am learning python")
 print("As a devops engineer, we can use in Lambda functions, cost optimization and so on")
+"""
+We can also use multiline strings
+"""
+
+print("This is how multiline string will work with three double quotes")
