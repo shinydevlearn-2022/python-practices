bfed6a8084aa78de00f3bf07f5554549b471bd9f
shinykuchi <kuchipudishiny1@gmail.com>
2024-10-17 01:15:10 +0530

    added datatypes

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+x = 10
+y = "Apple"
+z = 11.5
+a = ["apple", "banana", "pineapple"]
+b = {"name" : "shiny", "age" : "90"}
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print("the values are", x, y, z,a, sep='\n')
