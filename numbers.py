de066b986adfb114e0c0cbcb4b132cd360f10d34
shinykuchi <kuchipudishiny1@gmail.com>
2024-10-18 00:32:26 +0530

    Added python numbers

---
+++ numbers.py
@@ -0,0 +1,43 @@
+x = 10
+y = 20.4
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+
+a = 5
+b = 895948048029349884934
+print(type(a))
+print(type(b))
+
+x1 = 34e5
+x2 = -58.9
+print(type(x1))
+print(type(x2))
+
+y1 = 2j+56
+y2 = -53+9j
+print(type(y1))
+print(type(y2))
+
+
+d = 10
+e = 9.67
+f = 3+8j
+z1 = float(d)
+z2 = int(e)
+z3 = complex(e)
+z4 = complex(d)
+z5 = int(f.real)
+
+print(z1)
+print(z2)
+print(z3)
+print(z4)
+print(z5)
+
+print(type(z1))
+print(type(z2))
+print(type(z3))
+print(type(z4))
+print(type(z5))
